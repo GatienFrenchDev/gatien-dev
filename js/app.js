40e7837da8d921e95344ac8be26b703b0d0373a2
unknown <80203026+GatienFrenchDev@users.noreply.github.com>
2021-03-17 13:43:00 +0100

    update

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function calcul() {
+	var initial= document.getElementById("txtbox-initiale").value;
+	var futur= document.getElementById("txtbox-futur").value;
+	var jour = 0;
+	
+	while (Number(initial)< Number(futur)) {
+		var initial = Number(initial)*1.02;
+		var jour = Number(jour) + 1;
+	}
+	var final = Number(jour)-1
+	alert('Il faudra ' + final + " jours");
+}
